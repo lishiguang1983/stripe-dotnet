4c6ff8609c5e654dfb700c5f4e7c678f15d54d9b
Olivier Bellone <ob@stripe.com>
2017-10-12 14:20:55 +0200

    Adds support for subscription manual payments

---
+++ src/Stripe.net/Constants/StripeBilling.cs
@@ -0,0 +1,8 @@
+namespace Stripe
+{
+    public static class StripeBilling
+    {
+        public const string ChargeAutomatically = "charge_automatically";
+        public const string SendInvoice = "send_invoice";
+    }
+}
